e0b992e4ac83bd714e3bd6c8a796beccbce50e51
Tomas Jurkus <jurkust@gmail.com>
2025-01-29 18:54:58 +0200

    Fix.

--- a7jO3/keymap.c
+++ a7jO3/keymap.c
@@ -181,11 +181,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
     case MO(2):
-    case MO(3):
+    case LT(3,KC_ENTER):
       if (is_alt_tab_active && !record->event.pressed) {
         unregister_code(KC_LALT);
         is_alt_tab_active = false;
       }
+      break;
   }
   return true;
 }
